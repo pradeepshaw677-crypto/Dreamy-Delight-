58e7bd84b85b2752e9b802e65e289e2b68503694
pradeepshaw677-crypto <pradeepshaw677@gmail.com>
2026-06-15 22:15:55 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+// firebase.js
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDBhpcwAtd8gKVNtagusJ4FmFXK-7PiLQ",
+  authDomain: "dreamy-delight-27e16.firebaseapp.com",
+  databaseURL: "https://dreamy-delight-27e16-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "dreamy-delight-27e16",
+  storageBucket: "dreamy-delight-27e16.appspot.com",
+  messagingSenderId: "77481876440",
+  appId: "1:77481876440:web:7cf11156ee67b1e47ab0ad"
+};
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+// export database globally
+const db = firebase.database();
